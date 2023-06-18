61fc2f61c7145e5d46c640c3b56a9d5c3ad7dff1
Karim Abdallah <karim.a.s.khaled@gmail.com>
2023-06-18 02:20:51 +0000

    timer finished

--- scripts/english-quiz.js
+++ scripts/english-quiz.js
@@ -1,107 +1,137 @@
 const englishQuestions = [
   {
   question: "What is the difference between 'affect' and 'effect'?",
-  options: ["A) Affect is a verb, while effect is a noun.", "B) Effect is a verb, while affect is a noun.", "C) Both are adjectives with different meanings.", "D) Both are prepositions with different meanings."],
-  answer: "A) Affect is a verb, while effect is a noun."
+  options: ["Affect is a verb, while effect is a noun.", "Effect is a verb, while affect is a noun.", "Both are adjectives with different meanings.", "Both are prepositions with different meanings."],
+  answer: "Affect is a verb, while effect is a noun."
   },
   {
   question: "What is the origin of the word 'etymology'?",
-  options: ["A) Latin", "B) Greek", "C) French", "D) Germanic"],
-  answer: "B) Greek"
+  options: ["Latin", "Greek", "French", "Germanic"],
+  answer: "Greek"
   },
   {
   question: "What is a homophone?",
-  options: ["A) A word that sounds the same as another word but has a different spelling and meaning.", "B) A word that is spelled the same as another word but has a different pronunciation and meaning.", "C) A word that has multiple meanings.", "D) A word that has the same meaning as another word."],
+  options: ["A word that sounds the same as another word but has a different spelling and meaning.", "A word that is spelled the same as another word but has a different pronunciation and meaning.", "A word that has multiple meanings.", "A word that has the same meaning as another word."],
   answer: "A) A word that sounds the same as another word but has a different spelling and meaning."
   },
   {
   question: "What are some common idioms used in English?",
-  options: ["A) 'Break a leg'", "B) 'The ball is in your court'", "C) 'All of the above'", "D) 'The cat's out of the bag'"],
-  answer: "C) 'All of the above'"
+  options: ["'Break a leg'", "The ball is in your court", "All of the above", "The cat's out of the bag"],
+  answer: "'All of the above'"
   },
   {
   question: "What is the difference between 'who' and 'whom'?",
-  options: ["A) 'Who' is used as the subject of a sentence, while 'whom' is used as an object.", "B) 'Whom' is used as the subject of a sentence, while 'who' is used as an object.", "C) Both are interchangeable.", "D) 'Whom' is used to refer to animals or things, while 'who' is used to refer to people."],
-  answer: "A) 'Who' is used as the subject of a sentence, while 'whom' is used as an object."
+  options: ["'Who' is used as the subject of a sentence, while 'whom' is used as an object.", "'Whom' is used as the subject of a sentence, while 'who' is used as an object.", "Both are interchangeable.", "'Whom' is used to refer to animals or things, while 'who' is used to refer to people."],
+  answer: "'Who' is used as the subject of a sentence, while 'whom' is used as an object."
   },
   {
   question: "What is the plural of 'octopus'?",
-  options: ["A) Octopuses", "B) Octopi", "C) Octopodes", "D) Octopodes or octopuses are both correct."],
-  answer: "A) Octopuses"
+  options: ["Octopuses", "Octopi", "Octopodes", "Octopodes or octopuses are both correct."],
+  answer: "Octopuses"
   },
   {
   question: "What is the Oxford comma and why is it controversial?",
-  options: ["A) It is a comma used before the conjunction 'and' in a list of three or more items.", "B) Itis a comma used after the conjunction 'and' in a list of three or more items.", "C) It is a comma used before the last item in a list of three or more items.", "D) It is a comma used to separate clauses in a complex sentence."],
-  answer: "C) It is a comma used before the last item in a list of three or more items."
+  options: ["It is a comma used before the conjunction 'and' in a list of three or more items.", "It is a comma used after the conjunction 'and' in a list of three or more items.", "It is a comma used before the last item in a list of three or more items.", "It is a comma used to separate clauses in a complex sentence."],
+  answer: "It is a comma used before the last item in a list of three or more items."
   },
   {
   question: "What is the difference between 'its' and 'it's'?",
-  options: ["A) 'Its' is a possessive pronoun, while 'it's' is a contraction of 'it is'.", "B) 'It's' is a possessive pronoun, while 'its' is a contraction of 'it is'.", "C) Both have the same meaning.", "D) 'Its' is a contraction of 'it has', while 'it's' is a possessive pronoun."],
-  answer: "A) 'Its' is a possessive pronoun, while 'it's' is a contraction of 'it is'."
+  options: ["'Its' is a possessive pronoun, while 'it's' is a contraction of 'it is'.", "'It's' is a possessive pronoun, while 'its' is a contraction of 'it is'.", "Both have the same meaning.", "'Its' is a contraction of 'it has', while 'it's' is a possessive pronoun."],
+  answer: "'Its' is a possessive pronoun, while 'it's' is a contraction of 'it is'."
   },
   {
   question: "What is the difference between 'few' and 'a few'?",
-  options: ["A) 'Few' is used to indicate a small quantity, while 'a few' is used to indicate a slightly larger quantity.", "B) 'A few' is used to indicate a small quantity, while 'few' is usedto indicate a slightly larger quantity.", "C) Both have the same meaning.", "D) 'Few' is used to indicate a large quantity, while 'a few' is used to indicate a very small quantity."],
-  answer: "A) 'Few' is used to indicate a small quantity, while 'a few' is used to indicate a slightly larger quantity."
+  options: ["'Few' is used to indicate a small quantity, while 'a few' is used to indicate a slightly larger quantity.", "'A few' is used to indicate a small quantity, while 'few' is used to indicate a slightly larger quantity.", "Both have the same meaning.", "'Few' is used to indicate a large quantity, while 'a few' is used to indicate a very small quantity."],
+  answer: "'Few' is used to indicate a small quantity, while 'a few' is used to indicate a slightly larger quantity."
   },
   {
   question: "What is the difference between 'advice' and 'advise'?",
-  options: ["A) 'Advice' is a verb, while 'advise' is a noun.", "B) 'Advice' is a noun, while 'advise' is a verb.", "C) Both are nouns with different meanings.", "D) Both are verbs with different meanings."],
-  answer: "B) 'Advice' is a noun, while 'advise' is a verb."
+  options: ["'Advice' is a verb, while 'advise' is a noun.", "'Advice' is a noun, while 'advise' is a verb.", "Both are nouns with different meanings.", "Both are verbs with different meanings."],
+  answer: "'Advice' is a noun, while 'advise' is a verb."
   },
   {
   question: "What is a palindrome?",
-  options: ["A) A word that has multiple meanings depending on the context.", "B) A word that is spelled the same backwards as forwards.", "C) A word that sounds the same as another word but has a different spelling and meaning.", "D) A word that is spelled the same as another word but has a different pronunciation and meaning."],
-  answer: "B) A word that is spelled the same backwards as forwards."
+  options: ["A word that has multiple meanings depending on the context.", "A word that is spelled the same backwards as forwards.", "A word that sounds the same as another word but has a different spelling and meaning.", "A word that is spelled the same as another word but has a different pronunciation and meaning."],
+  answer: "A word that is spelled the same backwards as forwards."
   },
   {
   question: "What is the difference between 'imply' and 'infer'?",
-  options: ["A) 'Imply' is used to indicate a suggestion or hint, while 'infer' is used to draw a conclusion or make an educated guess.", "B) 'Infer' is used to indicate a suggestion or hint, while 'imply' is used to draw a conclusion or make an educated guess.", "C) Both are synonyms with the same meaning.", "D) 'Imply' is used to refer to people, while 'infer' is used to refer to things."],
-  answer: "A) 'Imply' is used to indicate a suggestion or hint, while 'infer' is used to draw a conclusion or make an educated guess."
+  options: ["'Imply' is used to indicate a suggestion or hint, while 'infer' is used to draw a conclusion or make an educated guess.", "'Infer' is used to indicate a suggestion or hint, while 'imply' is used to draw a conclusion or make an educated guess.", "Both are synonyms with the same meaning.", "'Imply' is used to refer to people, while 'infer' is used to refer to things."],
+  answer: "'Imply' is used to indicate a suggestion or hint, while 'infer' is used to draw a conclusion or make an educated guess."
   }
   ];
-
   let container = document.querySelector('#container')
-  // let submitBtn = document.querySelector('.submit')
+  let questionContainer = document.querySelector('.question')
+  let display = document.querySelector('.timer')
+  let submitBtn = document.createElement('button')
+  submitBtn.className = 'submit-btn'
+  submitBtn.textContent = 'Submit'
 
-// 5 random questions generated
-function generatingQuestions(englishQuestions){
+  function startTimer(duration, display) {
+    let timer = duration;
+    let minutes, seconds;
+  
+    let countdown = setInterval(function() {
+      minutes = Math.floor(timer / 60);
+      seconds = timer % 60;
+  
+      // Add leading zeros if needed
+      minutes = minutes < 10 ? "0" + minutes : minutes;
+      seconds = seconds < 10 ? "0" + seconds : seconds;
+  
+      // Display the time remaining
+      display.textContent = minutes + ":" + seconds;
+  
+      if (--timer < 0) {
+        // Timer has reached zero, do something
+        clearInterval(countdown);
+        alert("Timer has ended!");
+      }
+    }, 1000);
+  }
+  let countdownDuration = 60 * 1; // 5 minutes in seconds
+
+  // 5 random questions generated
+  function generatingQuestions(englishQuestions){
   let questionsArr = []
   let answersArr = []
   let optionsArr = []
   let i = 0
-  while(i < 5){
+  while(i < 1){
       let randomNumber = Math.floor(Math.random() * englishQuestions.length)
       if(questionsArr.indexOf(englishQuestions[randomNumber].question) == -1){
         questionsArr.push(englishQuestions[randomNumber].question)
+        // console.log(questionsArr)
         answersArr.push(englishQuestions[randomNumber].answer)
+        // console.log(answersArr)
         optionsArr.push(englishQuestions[randomNumber].options)
+        // console.log(optionsArr)
         i++
       }
-      
+      displayeQuestions(questionsArr,optionsArr,answersArr,)
     }
-    displayeQuestions(questionsArr,answersArr,optionsArr)
   }
-
 
 function displayeQuestions(questions,options,answer){
-  for(let question of questions){
+  for(let i = 0; i < questions.length; i++){
     let div = document.createElement('div')
-    div.className = `question-${questions.indexOf(question) + 1} questions`
-    let questionsLabel = document.createElement('h3')
-    questionsLabel.innerText = question
+    let questionsLabel = document.createElement('h2')
+    questionsLabel.innerText = questions[i]
     div.appendChild(questionsLabel)
-    container.appendChild(div)
-
-    for(let option of options){
-      let theOptions = document.createElement('input')
-      theOptions.style.display = 'block'
-      theOptions.innerText = option
-      theOptions.type = 'radio'
-      div.appendChild(theOptions)
-    }
+    questionContainer.appendChild(div)
+    console.log(options)
+      for(let i = 0; i < options.length; i++){
+        let label = document.createElement('label')
+        let theOptions = document.createElement('input')
+        theOptions.type = 'radio'
+        label.style.display = 'block'
+        label.appendChild(theOptions)
+        label.appendChild(document.createTextNode(options));
+        div.appendChild(label)
+      }
   }
-
+  questionContainer.appendChild(submitBtn)
 }
 
 generatingQuestions(englishQuestions)
+startTimer(countdownDuration, display);
